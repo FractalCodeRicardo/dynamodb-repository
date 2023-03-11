35749d175aec4c739a9bcad072e013047059b0fc
Ricardo Serrato B <rsbse@hotmail.com>
2023-03-11 12:12:35 -0600

    Changes

--- dynamodb/dynamodb.py
+++ dynamodb/dynamodb.py
@@ -1,5 +1,6 @@
 import ctypes
 from dynamodb.repository import Repository
+from boto3.dynamodb.conditions import ConditionBase
 from typing import Type, TypeVar, List, Dict
 
 import boto3
@@ -14,13 +15,23 @@ class DynamoDB(Repository[T]):
                  cls: Type[T]
                  ):
         
-        client = boto3.resource("dynamodb", endpoint_url = url)
+        client = DynamoDB.__get_resource(url);
         self.cls = cls
         self.table = client.Table(table_name);
     
+
+    @staticmethod
+    def __get_client(url):
+        return boto3.client("dynamodb", endpoint_url = url)
+    
+    @staticmethod
+    def __get_resource(url):
+        return boto3.resource("dynamodb", endpoint_url = url)
+
+
     @staticmethod
     def create_table(url, table_name, key_schema, attributes, capacity):
-        client = boto3.resource("dynamodb", endpoint_url = url)
+        client = DynamoDB.__get_client(url);
         client.create_table(
             TableName = table_name,
             KeySchema = key_schema,
@@ -30,8 +41,15 @@ class DynamoDB(Repository[T]):
     
     @staticmethod
     def delete_table(url, table_name,):
-        client = boto3.resource("dynamodb", endpoint_url = url)
+        client = DynamoDB.__get_resource(url);
         client.Table(table_name).delete()
+
+
+    @staticmethod
+    def table_exists(url, table_name):
+        client = DynamoDB.__get_client(url);
+        response = client.list_tables()
+        return table_name in response["TableNames"]
 
 
     def create_from(self, dictionary: Dict) -> T:
@@ -50,14 +68,14 @@ class DynamoDB(Repository[T]):
         return new_array
 
 
-    def find(self, condition) -> List[T]:
+    def find(self, condition: ConditionBase) -> List[T]:
         res = self.table.query(KeyConditionExpression = condition)
         items = res["Items"]
         array = self.create_from_array(items)
         return array
     
 
-    def find_first(self, condition) -> T:
+    def find_first(self, condition: ConditionBase) -> T:
         res = self.table.query(KeyConditionExpression = condition)
         items = res["Items"]
 
@@ -84,35 +102,46 @@ class DynamoDB(Repository[T]):
 
 
     def get_update_expresion(self, columns: List):
-        expressions = map(lambda i: i + "= :" + i, columns)
+        expressions = map(lambda i: "#" + i + "= :" + i, columns)
         expressions = list(expressions)
         return "SET " + ",".join(expressions)
 
 
-    def get_expression_attributes(self, columns: List, model: Dict):
+    def get_expression_attributes_values(self, columns: List, model: Dict):
         values = {}
         for c in columns:
             values[":" + c] = model[c]
         return values
+    
+    def get_expression_attributes_names(self, columns: List):
+        values = {}
+        for c in columns:
+            values["#" + c] = c
+        return values
 
 
-    def update(self, key: Dict, model: Dict) -> T:
-        columns = self.columns_without_keys(key, model)
+    def update(self, key: Dict, model: T) -> T:
+        model_dict = model.__dict__
+        columns = self.columns_without_keys(key, model_dict)
         update_expression = self.get_update_expresion(columns)
-        expression_values = self.get_expression_attributes(columns, model)
+        expression_values = self.get_expression_attributes_values(columns, model_dict)
+        expression_names = self.get_expression_attributes_names(columns)
 
-        resp = self.table.update_item (
+        self.table.update_item (
             Key = key,
             UpdateExpression = update_expression,
             ExpressionAttributeValues = expression_values,
+            ExpressionAttributeNames = expression_names,
             ReturnValues = "UPDATED_NEW"
         )
         return model
 
-    def delete(self, key):
-        self.table.delete_item(key)
 
-    def any(self, condition):
+    def delete(self, key: Dict):  
+        self.table.delete_item(Key = key)
+
+
+    def any(self, condition: ConditionBase):
         res = self.table.query(KeyConditionExpression = condition)
         items = res["Items"]
         return len(items) > 0
--- tests/test_dynamodb.py
+++ tests/test_dynamodb.py
@@ -6,7 +6,7 @@ dynamodb_endpoint = "http://localhost:1111"
 dynamodb_table = "Clients"
 
 
-class Client:
+class Model:
     def __init__(self):
         self.Key = ""
         self.Name = ""
@@ -14,11 +14,31 @@ class Client:
 
 class DynamoDBTest(TestCase):
 
-    def test_delete(self):
-        DynamoDB.delete_table(dynamodb_endpoint, "no existe")
+    def deleteTableIfExists(self):
+        if DynamoDB.table_exists(dynamodb_endpoint, dynamodb_table):
+            DynamoDB.delete_table(dynamodb_endpoint, dynamodb_table)
+
+    def createTable(self):
+        DynamoDB.create_table(
+            url=dynamodb_endpoint,
+            table_name=dynamodb_table,
+            key_schema=[
+                {'AttributeName': 'Key', 'KeyType': 'HASH'}
+            ],
+            attributes=[
+                {'AttributeName': 'Key', 'AttributeType': 'S'},
+            ],
+            capacity={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            })
+
+    def setUp(self) -> None:
+        self.deleteTableIfExists()
+        self.createTable()
 
     def get_repo(self) -> DynamoDB:
-        repo = DynamoDB(dynamodb_endpoint, dynamodb_table, Client)
+        repo = DynamoDB(dynamodb_endpoint, dynamodb_table, Model)
         return repo
 
     def test_create_from(self):
@@ -34,16 +54,99 @@ class DynamoDBTest(TestCase):
         self.assertEqual(obj[0].true, True)
 
     def test_find(self):
-        self.get_repo().find(Key("Key").eq("1"))
-
-    def test_get_first(self):
-        condition = Key("Key").eq("1")
         repo = self.get_repo()
-        res = repo.find_first(condition)
+
+        model = Model()
+        model.Key = "1"
+
+        repo.insert(model)
+        res = repo.find(Key("Key").eq("1"))
+
+        self.assertTrue(len(res) > 0)
+        self.assertEqual(res[0].Key, "1")
+
+    def test_find_first(self):
+        repo = self.get_repo()
+
+        model = Model()
+        model.Key = "1"
+
+        repo.insert(model)
+        inserted_model = repo.find_first(Key("Key").eq("1"))
+
+        self.assertIsNotNone(inserted_model)
+        self.assertEqual(inserted_model.Key, "1")
 
     def test_insert(self):
-        client = Client()
-        client.Key = "1"
-        client.Name = "Name"
         repo = self.get_repo()
-        repo.insert(client)
+
+        model = Model()
+        model.Key = "1"
+        model.Name = "Name"
+        repo.insert(model)
+
+    def test_columns_without_keys(self):
+        keys = {"Key": ""}
+        model = Model()
+        dict_model = model.__dict__
+
+        repo = self.get_repo()
+        columns = repo.columns_without_keys(keys, dict_model)
+
+        self.assertTrue(not "Key" in columns)
+        self.assertTrue("Name" in columns)
+
+    def test_get_update_expression(self):
+        columns = ["column1", "column2"]
+        repo = self.get_repo()
+        expression = repo.get_update_expresion(columns)
+        self.assertTrue(
+            expression == "SET #column1= :column1,#column2= :column2")
+
+    def test_get_expression_attributes(self):
+        model = {"Name": "name"}
+
+        columns = ["Name"]
+        repo = self.get_repo()
+        expression = repo.get_expression_attributes_values(columns, model)
+        self.assertTrue(expression[":Name"] == "name")
+
+    def test_update(self):
+        model = Model()
+        model.Key = "1"
+        model.Name = "name"
+
+        repo = self.get_repo()
+        repo.insert(model)
+
+        model.Name = "updatedname"
+        repo.update(key={"Key": "1"}, model=model)
+
+        model = repo.find_first(Key("Key").eq("1"))
+
+        self.assertIsNotNone(model)
+        self.assertEqual(model.Name, "updatedname")
+
+    def test_delete(self):
+        model = Model()
+        model.Key = "1"
+
+        repo = self.get_repo()
+        repo.insert(model)
+
+        repo.delete({"Key": "1"})
+
+        model = repo.find_first(Key("Key").eq("1"))
+
+        self.assertIsNone(model)
+
+    def test_any(self):
+        model = Model()
+        model.Key = "1"
+
+        repo = self.get_repo()
+        repo.insert(model)
+
+        has_elements = repo.any(Key("Key").eq("1"))
+
+        self.assertTrue(has_elements)
